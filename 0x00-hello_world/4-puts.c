a155285ef0fe161c562693b04c6643b3d36e92e0
Muncake44 <swmugwe@gmail>
2023-07-13 15:55:07 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/**
+ * main - Prints Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
