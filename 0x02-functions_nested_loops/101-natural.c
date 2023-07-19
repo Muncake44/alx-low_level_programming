bfc8edab2208a4392ac45a14747cb5f55d2dd1e4
Muncake44 <swmugwe@gmail>
2023-07-19 15:12:55 -0700

    push

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-        int sum = 0;
-        int i;
+	int sum = 0;
+	int i;
 
-        for (i = 0; i < 1024; i++)
-        {
-                if (i % 3 == 0 || i % 5 == 0)
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
                         sum = sum + i;
         }
         printf("%d\n", sum);
