47249f20cdce48e2bcaaaa0ca06ba3f16976f150
Muncake44 <swmugwe@gmail>
2023-07-31 07:23:53 -0700

    print diagsum

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+
  /**
-  * print_chessboard - prints the chessboard
-  * @a: array
-  * Return 0 (Success)
+  * print_chessboard - prints chessboard
+  * @a: is an array
+  * Return: Always 0 (Success)
   */
 void print_chessboard(char (*a)[8])
 {
