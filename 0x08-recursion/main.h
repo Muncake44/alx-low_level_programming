8d913488b821207bfe669a2d7fca3a4b4ba675cd
Muncake44 <swmugwe@gmail>
2023-08-02 07:13:01 -0700

    factorial

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _print_rev_recursion(char *s);
-void _print_rev_recursion(char *s);
+int factorial(int n);
 int _strlen_recursion(char *s);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
